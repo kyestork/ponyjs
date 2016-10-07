fb12521837649aa74dd1f2203f698f9919cdd6cc
Kage Edwards <kagemykel.edwards@gmail.com>
2016-10-07 15:06:03 -0700

    Update

--- pony.js
+++ pony.js
@@ -1,46 +1,4 @@
 var topContext = this,
-    debounce = function(fn, delay) {
-        var timer = null;
-        return function() {
-            var context = this,
-                args = arguments;
-            clearTimeout(timer);
-            timer = setTimeout(function() {
-                fn.apply(context, args);
-            }, delay);
-        };
-    },
-    bind = function() {
-        if (arguments[1].startsWith('#')) {
-            $(arguments[1]).on(arguments[0], arguments[2]);
-        } else {
-            $(topContext).on(arguments[0], arguments[1], arguments[2]);
-        }
-    },
-    unbind = function() {
-        if (arguments[1].startsWith('#')) {
-            $(arguments[1]).off(arguments[0], arguments[2]);
-        } else {
-            $(topContext).off(arguments[0], arguments[1], arguments[2]);
-        }
-    },
-    lesson = function(key, fn) {
-        if (typeof fn === 'function') {
-            pony.lessons[key] = fn;
-        }
-        return pony.lessons[key] || (function() {});
-    },
-    fetch = function(route, parcel, callback, fallback) {
-        var output, parcel = parcel || {};
-        parcel['_token'] = $('meta[name=formtoken]').attr('content');
-        $.post(route, parcel).done(function(data) {
-            if (data.success) {
-                callback.call(this, data.data);
-            } else {
-                (fallback || (function() {})).call(this);
-            }
-        });
-    },
     pony = {
         _offset: 0,
         _queue: [],
@@ -71,6 +29,48 @@ var topContext = this,
         },
         init: function() {
             (topContext.addEventListener) ? topContext.addEventListener('load', function() { pony.process(); }) : topContext.attachEvent('onload', function() { pony.process(); });
+        },
+        tame: function(fn, delay) {
+            var timer = null;
+            return function() {
+                var context = this,
+                    args = arguments;
+                clearTimeout(timer);
+                timer = setTimeout(function() {
+                    fn.apply(context, args);
+                }, delay);
+            };
+        },
+        bind: function() {
+            if (arguments[1].startsWith('#')) {
+                $(arguments[1]).on(arguments[0], arguments[2]);
+            } else {
+                $(topContext).on(arguments[0], arguments[1], arguments[2]);
+            }
+        },
+        unbind: function() {
+            if (arguments[1].startsWith('#')) {
+                $(arguments[1]).off(arguments[0], arguments[2]);
+            } else {
+                $(topContext).off(arguments[0], arguments[1], arguments[2]);
+            }
+        },
+        lesson: function(key, fn) {
+            if (typeof fn === 'function') {
+                pony.lessons[key] = fn;
+            }
+            return pony.lessons[key] || (function() {});
+        },
+        fetch: function(route, parcel, callback, fallback) {
+            var output, parcel = parcel || {};
+            parcel['_token'] = $('meta[name=formtoken]').attr('content');
+            $.post(route, parcel).done(function(data) {
+                if (data.success) {
+                    callback.call(this, data.data);
+                } else {
+                    (fallback || (function() {})).call(this);
+                }
+            });
         }
     };
 
